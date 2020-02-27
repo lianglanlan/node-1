bcd3aa8cbfb943c56c02094c866600afcf02f328
lianglanlan <799679063@qq.com>
2020-02-27 22:47:27 +0800

    简化代码

--- main/http.js
+++ main/http.js
@@ -7,26 +7,25 @@ const server = http.createServer()
 
 const wwwDir = '/Users/lianglanlan/Desktop/code/study/node/使用nodejs模拟apache功能/www'
 
-server.on('request', (req, res) => {
-    fs.readFile('./template.html', (err, data) => {
-        if (err) {
-            return res.end('404 Not Found')
-        }
-        fs.readdir(wwwDir, (err, files) => {
+http
+    .createServer((req, res) => {
+        fs.readFile('./template.html', (err, data) => {
             if (err) {
-                return res.end('cannot find www dir')
+                return res.end('404 Not Found')
             }
-            // 使用模板引擎替换data中内容
-            data = nunjucks.renderString(data.toString(), {
-                title: '123',
-                files: files
+            fs.readdir(wwwDir, (err, files) => {
+                if (err) {
+                    return res.end('cannot find www dir')
+                }
+                // 使用模板引擎替换data中内容
+                data = nunjucks.renderString(data.toString(), {
+                    title: '123',
+                    files
+                })
+                res.end(data)
             })
-            res.end(data)
         })
-
     })
-})
-
-server.listen(3010, () => {
-    console.log('running...')
-})
+    .listen(3010, () => {
+        console.log('running...')
+    })
